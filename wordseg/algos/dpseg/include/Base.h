8ebfe96b884fd5b9a44bf68df1c04ce4cea9879c
Mathieu Bernard <mathieu_bernard@laposte.net>
2017-10-04 18:15:42 +0200

    cleanup useless licence headers

--- wordseg/algos/dpseg/include/Base.h
+++ wordseg/algos/dpseg/include/Base.h
@@ -1,21 +1,3 @@
-/*
-  Copyright 2007 Mark Johnson, Brown University
-  Copyright 2009 Sharon Goldwater
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #ifndef _BASE_H_
 #define _BASE_H_
 
@@ -663,4 +645,4 @@ typedef UnigramsT<P0> Unigrams;
 typedef BigramsT<Unigrams> Bigrams;
 
 
-#endif
+#endif  // _BASE_H_
