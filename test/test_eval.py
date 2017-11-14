9c926c879503d91ad28aec3db05bceed373416f7
Mathieu Bernard <mathieu_bernard@laposte.net>
2017-11-14 15:16:58 +0100

    updated test

--- test/test_eval.py
+++ test/test_eval.py
@@ -1,7 +1,8 @@
 """Test of the wordseg_eval module"""
 
 from wordseg.evaluate import read_data, evaluate
-from . import gold
+from wordseg.algos import tp
+from . import prep, gold
 
 
 def test_read_data():
@@ -11,6 +12,16 @@ def test_read_data():
         ['a b', 'c']) == (['ab', 'c'], [{(0, 1), (1, 2)}, {(0, 1)}])
 
 
-def test_evaluate(gold):
+def test_evaluate_silly(gold):
     for v in evaluate(gold, gold).values():
         assert v == 1.0
+
+
+def test_evaluate_segmented(prep, gold):
+    s = tp.segment(prep)
+    e = evaluate(s, gold)
+
+    # make sure we have different scores for type and token
+    assert e['type_fscore'] != e['token_fscore']
+    assert e['type_precision'] != e['token_precision']
+    assert e['type_recall'] != e['token_recall']
